abfb41c400b11ed0ae851dc4392e27640cc80263
Trịnh Duy Khang <khang.trinh@bankexpress.vn>
2019-05-15 17:20:21 +0700

    initialize

---
+++ Drink.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Drinks
+{
+    public class Drink
+    {
+        public string Name { get; set; }
+        public DateTime ManufacturedOn { get; set; }
+        public List<string> With { get; set; }
+
+        internal static Drink ColdVodka()
+        {
+            return new Drink {
+                Name = "Vodka",
+                ManufacturedOn = DateTime.Now.Date,
+                With = new List<string> {"Ice", "Lemon"}
+            };
+        }
+
+        internal static Drink WarmTea()
+        {
+            return new Drink {
+                Name = "Black Tea",
+                ManufacturedOn = DateTime.Now.Date,
+                With = new List<string> {"Cookies"}
+            };
+        
+        }
+
+        internal static Drink OrangeJuice()
+        {
+            return new Drink {
+                Name = "Orange juice",
+                ManufacturedOn = DateTime.Now.Date,
+                With = new List<string> {}
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Drinks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Drink vodka = Drink.ColdVodka();
+            Drink blackTea = Drink.WarmTea();
+            Drink orangeJuice = Drink.OrangeJuice();
+            List<Drink> drinks = new List<Drink> {vodka, blackTea, orangeJuice};
+            ParameterExpression obj = Expression.Parameter(typeof(Drink), "Drink");
+            MemberExpression prop = Expression.Property(obj, "Name");
+            ConstantExpression cons = Expression.Constant("Vodka", typeof(string));
+            BinaryExpression expression = Expression.Equal(prop, cons);
+            var isVodka = Expression.Lambda<Func<Drink, bool>>(expression, new [] {obj});
+            System.Console.WriteLine(isVodka.Parameters.Count);
+        }
+    }
+}
