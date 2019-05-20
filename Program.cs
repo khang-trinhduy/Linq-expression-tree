49a8e38f8ba2893c873e03cce32bb337790258c9
Trịnh Duy Khang <khang.trinh@bankexpress.vn>
2019-05-20 19:09:33 +0700

    linq dynamic & linq core

--- Program.cs
+++ Program.cs
@@ -1,26 +1,99 @@
 ﻿using System;
-using Drinks;
-using System.Linq;
 using System.Collections.Generic;
-using System.Linq.Expressions;
+using System.Reflection;
+using System.ComponentModel;
+using System.Linq.Dynamic.Core;
+using Class;
+using System.Linq;
 
 namespace Linq
 {
+    // public class Adult : ISpecifications<User>
+    // {
+    //     public IsSatisfiedBy(User user)
+    //     {
+    //         return user.Age >= 19;
+    //     }
+    // }
     class Program
     {
+        
+        private static readonly IQueryable<User> userData = UserDataSeed().AsQueryable();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Drink vodka = Drink.ColdVodka();
-            Drink blackTea = Drink.WarmTea();
-            Drink orangeJuice = Drink.OrangeJuice();
-            List<Drink> drinks = new List<Drink> {vodka, blackTea, orangeJuice};
-            ParameterExpression obj = Expression.Parameter(typeof(Drink), "Drink");
-            MemberExpression prop = Expression.Property(obj, "Name");
-            ConstantExpression cons = Expression.Constant("Vodka", typeof(string));
-            BinaryExpression expression = Expression.Equal(prop, cons);
-            var isVodka = Expression.Lambda<Func<Drink, bool>>(expression, new [] {obj});
-            System.Console.WriteLine(isVodka.Parameters.Count);
+            var user = userData.Where("FirstName == @1", 2, "Kevin").ToList();
+            foreach (var u in user)
+            {
+                System.Console.WriteLine(u.LastName);
+            }
+        }
+        private static List<User> UserDataSeed()
+        {
+            
+            return new List<User>
+            {
+                new User{ ID = 1, FirstName = "Kevin", LastName = "Garnett", Age = 18},
+                new User{ ID = 2, FirstName = "Stephen", LastName = "Curry", Age = 19},
+                new User{ ID = 3, FirstName = "Kevin", LastName = "Durant", Age = 20}
+            };
+            
+
+        }
+
+        // private static Func<User, bool> GetDynamicQueryWithExpresionTrees(string propertyName, string val)
+        // {
+        //     //x =>
+        //     var param = Expression.Parameter(typeof(User), "x");
+
+        //     #region Convert to specific data type
+        //     MemberExpression member = Expression.Property(param, propertyName);
+        //     UnaryExpression valueExpression = GetValueExpression(propertyName, val, param);
+        //     #endregion
+        //     //val ("Curry")
+        //     Expression body = Expression.Equal(member, valueExpression);
+        //     //x => x.LastName == "Curry"
+        //     var final = Expression.Lambda<Func<User, bool>>(body: body, parameters: param);
+        //     return final.Compile();
+        // }
+
+        // private static UnaryExpression GetValueExpression(string propertyName, string val, ParameterExpression param)
+        // {
+        //     var member = Expression.Property(param, propertyName);
+        //     var propertyType = ((PropertyInfo)member.Member).PropertyType;
+        //     var converter = TypeDescriptor.GetConverter(propertyType);
+
+        //     if (!converter.CanConvertFrom(typeof(string)))
+        //         throw new NotSupportedException();
+
+        //     var propertyValue = converter.ConvertFromInvariantString(val);
+        //     var constant = Expression.Constant(propertyValue);
+        //     return Expression.Convert(constant, propertyType);
+        // }
+
+        private static Func<User, bool> GetDynamicFunc(string property, string val)
+        {
+            Func<User, bool> exp = (t) => true;
+            switch (property.ToLower())
+            {
+
+                case "id":
+                    exp = d => d.ID == Convert.ToInt32(val);
+                    break;
+                case "firstname":
+                    exp = d => d.FirstName == val;
+                    break;
+                case "lastname":
+                    exp = d => d.LastName == val;
+                    break;
+                default:
+                    throw new InvalidCastException(nameof(property));
+
+
+            }
+            return exp;
         }
     }
+
 }
+
---
+++ User.cs
@@ -0,0 +1,10 @@
+namespace Class
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
